2ace8c3d5341c8532d4598968f41ac6f548a0f93
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-07-20 20:18:01 -0300

    starting project

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+import sys
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+
+    try:
+        if '.txt' not in path_file:
+            print("Formato inválido", file=sys.stderr)
+
+        file = open(path_file, 'r')
+        content = [line.replace("\n", "") for line in file.readlines()]
+
+        return content
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
