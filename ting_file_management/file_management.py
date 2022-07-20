8286b56723f89b4674ed4656e3f8c750b06e53ef
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-07-20 20:25:55 -0300

    not javascript again

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -9,8 +9,9 @@ def txt_importer(path_file):
             print("Formato inválido", file=sys.stderr)
 
         file = open(path_file, 'r')
-        content = [line.replace("\n", "") for line in file.readlines()]
+        content = file.readlines()
+        formated = [line.replace("\n", "") for line in content]
 
-        return content
+        return formated
     except FileNotFoundError:
         print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
